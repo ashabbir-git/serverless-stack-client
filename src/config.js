c5d2a1f25d69c30d46a1a9e8b69c881798fbb68e
Shabbir <ahmad.shabbir@txu.com>
2020-10-09 19:05:58 -0500

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-uploads-shabbir"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://2lnplv3el0.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_CVHcgUW5l",
+    APP_CLIENT_ID: "5rokq18l5qmfgo874fibes6oen",
+    IDENTITY_POOL_ID: "us-east-1:376c8767-b86f-4a32-8bf9-b503bff7da34"
+  }
+};
